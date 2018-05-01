9a6fd718ee27b25c26aef261ca428e15ac085259
gbengaPS <ioyetade@gmail.com>
2018-05-01 16:56:17 +0100

    chore(code-base) delete initial files

--- server/models/base_model.py
+++
@@ -1,8 +0,0 @@
-from server.app import DB
-
-
-class BaseModel:
-    def save(self):
-        """Save data to database"""
-        DB.session.add(self)
-        DB.session.commit()
--- server/routes.py
+++
@@ -1,8 +0,0 @@
-from server.controllers.users.authentication.signup import UserSignup
-from server.controllers.users.authentication.login import UserLogin
-
-
-
-def router(api):
-    api.add_resource(UserSignup, '/api/user/signup')
-    api.add_resource(UserLogin, '/api/user/login')
